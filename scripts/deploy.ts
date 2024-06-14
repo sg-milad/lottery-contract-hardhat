aac15558efd6f12b6ad5fc6e8e05566a37e84e99
milad <miladbachari14@gmail.com>
2024-06-14 14:23:19 +0330

    style: format

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,12 +1,14 @@
 import hre from "hardhat";
 
 async function main() {
-  const LotteryManager = await hre.viem.deployContract("LotteryManager")
+  const LotteryManager = await hre.viem.deployContract("LotteryManager");
   console.log("manager", await LotteryManager.read.manager());
 
   console.log(`lottery manager contract address: ${LotteryManager.address}`);
   console.log(`manager address: ${await LotteryManager.read.manager()}`);
-  console.log(`lottery contract address: ${await LotteryManager.read.lotteryAddress()}`);
+  console.log(
+    `lottery contract address: ${await LotteryManager.read.lotteryAddress()}`,
+  );
 }
 
 main().catch((error) => {
