920744292542061191901433e7bb64a66ee37776
milad <miladbachari14@gmail.com>
2024-06-17 12:28:40 +0330

    refactor: deploy mock

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,9 +1,19 @@
-import hre from "hardhat";
+import hre, { network } from "hardhat";
 
 async function main() {
     const LotteryManager = await hre.viem.deployContract("LotteryManager");
     console.log("manager", await LotteryManager.read.manager());
 
+    if (network.name === "hardhat" || network.name === "localhost") {
+        console.log("Local network detected! Deploying mocks...");
+
+        const mock = await hre.viem.deployContract("VRFCoordinatorV2Mock", ["100000000000000000", "1000000000"]);
+
+        console.log("Mocks Deployed!");
+
+        console.log("----------------------------------");
+        console.log("mock contract address:", mock.address);
+    }
     console.log(`lottery manager contract address: ${LotteryManager.address}`);
     console.log(`manager address: ${await LotteryManager.read.manager()}`);
     console.log(`lottery contract address: ${await LotteryManager.read.lotteryAddress()}`);
